cc55ace56a60ae011cb2f5de9d42d8688d23d33f
ellabentovim <Ella@bentovim.com>
2025-09-13 12:30:55 +0300

    add: main.py file

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from menu import show_menu
+from utils import show_users,ask_name
 
 def main():
     print("welcome to the program!")
-    show_menu()
+    ask_name()
+    while(True):
+        show_menu()
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            show_users()
+        elif choice == 2:
+            print("exiting the program. Goodbye!")
+            return
+        else:
+            print("Invalid choice. Please try again.")
 
 if __name__ == '__main__':
     main()
